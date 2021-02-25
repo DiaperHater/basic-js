47f930fd692a4e1e79bf338c5f116dd797c7d7e5
314ronin <pownmymail@gmail.com>
2021-02-25 16:55:37 +0300

    feat: implement simple-chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,25 +1,52 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
+
+  chain: [],
+
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    return this.chain.length;
   },
+
   addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+
+    this.chain.push('( ' + String(value) + ' )~~');
+
+    return this;
   },
+
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+
+    //elements starts from 1 instead 0
+    position = position - 1;
+
+    if(position % 1 != 0 || this.chain[position] == undefined) {
+      this.reset();
+      throw Error();
+    }
+
+    this.chain.splice(position, 1);
+
+    return this;
   },
+
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.chain.reverse();
+    return this;
   },
+
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    const lastIndex = this.getLength() - 1;
+    let lastElement = this.chain[lastIndex];
+    this.chain[lastIndex] = lastElement.replace('~~', '');
+
+    const str = this.chain.join('');
+    this.reset();
+    return str;
+  },
+
+  reset() {
+    this.chain.splice(0);
   }
 };
 
