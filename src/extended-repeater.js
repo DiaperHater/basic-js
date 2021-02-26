0271279cfa55818651f9b7905d368e1b82fedb99
314ronin <pownmymail@gmail.com>
2021-02-26 22:15:02 +0300

    feat: implement extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,52 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+	if(options.addition === null) {
+		options.addition = String(options.addition);
+	}
+	if(!options.additionRepeatTimes) {
+		options.additionRepeatTimes = 1;
+	}
+	let addition = mult(options.addition, options.additionRepeatTimes);
+
+	if(!options.additionSeparator) {
+		options.additionSeparator = '|';
+	}
+	addition = insertSeparator(addition, options.additionSeparator);
+
+	str += addition;
+
+	if(!options.repeatTimes) {
+		options.repeatTimes = 1;
+	}
+	str = mult(str, options.repeatTimes);
+
+	if(!options.separator) {
+		options.separator = '+';
+	}
+	str = insertSeparator(str, options.separator);
+
+
+	return str;
 };
   
+
+function insertSeparator(arr, sep) {
+	for(i = 0; i < arr.length; i++) {
+		if(i % 2 != 0) {
+			arr.splice(i, 0, sep);
+		}
+	}
+
+	return arr.join('');
+}
+
+function mult(s, n) {
+	let arr = [];
+
+	for(i = 0; i < n; i++) {
+		arr.push(s);
+	}
+
+	return arr;
+}
